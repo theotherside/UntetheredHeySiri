378f52af3118248f7c2a4edc7fabdbda4172eacd
Hamza Sood <hamzasood@Hamzas-MacBook-Pro.local>
2014-10-24 18:10:43 +0100

    Initial Commit

---
+++ UntetheredHeySiri/UntetheredHeySiri.h
@@ -0,0 +1,13 @@
+//
+//  UntetheredHeySiri.h
+//  UntetheredHeySiri
+//
+//  Created by Hamza Sood on 24/10/2014.
+//  Copyright (c) 2014 Hamza Sood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UntetheredHeySiri : NSObject
+
+@end
---
+++ UntetheredHeySiri/UntetheredHeySiri.m
@@ -0,0 +1,13 @@
+//
+//  UntetheredHeySiri.m
+//  UntetheredHeySiri
+//
+//  Created by Hamza Sood on 24/10/2014.
+//  Copyright (c) 2014 Hamza Sood. All rights reserved.
+//
+
+#import "UntetheredHeySiri.h"
+
+@implementation UntetheredHeySiri
+
+@end
